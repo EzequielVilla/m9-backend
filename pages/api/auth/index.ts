8d89e03e22d960bd4907ba4ecd46ca761491a9ee
EzequielVilla <ezequiel.n.villa@gmail.com>
2022-02-20 01:47:36 -0300

    add cors test to auth

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -4,6 +4,17 @@ import { sendCodeEmail } from "lib/sendgrid";
 import methods from "micro-method-router"
 import * as yup from "yup"
 import { yupAuthIndexBody } from "lib/middlewares";
+// import initMiddleware from "lib/init-middleware";
+import NextCors from 'nextjs-cors';
+
+// Initialize the cors middleware
+// const cors = initMiddleware(
+//   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+//   Cors({
+//     // Only allow requests with GET, POST and OPTIONS
+//     methods: ['GET', 'POST', 'OPTIONS'],
+//   })
+// )
 
 const bodySchema = yup.object().shape({
     email: yup.string().required(),
@@ -12,8 +23,13 @@ const bodySchema = yup.object().shape({
 
 
 async function postHandler(req:NextApiRequest, res:NextApiResponse){
-    
-    
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+     });
+    // await cors(req, res)
     const {email} = req.body
     await findOrCreateAuth(email);
     const auth = await sendCode(email)     
